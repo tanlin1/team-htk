0a63570a54fe77c442888651f7dd2367aff7b789
tanlin <2855798226@qq.com>
2014-11-29 07:13:41 +0800

    由于时间紧迫，私信页面的内容没有更新，目前是跟通知页一样（因为Adapter都是一样的）
    接下来：更新私信页面，主页点击图片实现QQ似的弹屏，占满整个屏幕，弹出效果，展示我没考虑。能点击它貌似很不容容易。

--- src/com/htk/moment/ui/UserMainCoreActivity.java
+++ src/com/htk/moment/ui/UserMainCoreActivity.java
@@ -4,30 +4,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerAdapter;
 import android.util.Log;
 import android.view.*;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.*;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import utils.android.photo.CameraActivity;
 import utils.android.photo.LocalPictureLibrary;
-import utils.view.fragment.*;
-import utils.view.IndexPullRefreshListView;
 import utils.view.NotFilingViewPager;
+import utils.view.fragment.IndexFragment;
+import utils.view.fragment.MeFragment;
+import utils.view.fragment.MessageFragment;
+import utils.view.fragment.SearchFragment;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 
 
 /**
@@ -497,243 +495,4 @@ public class UserMainCoreActivity extends FragmentActivity {
 		disappear();
 		buttonIsOnscreen = false;
 	}
-
-	private class MyContentListViewAdapter extends BaseAdapter {
-
-		private List<HashMap<String, Object>> listData;
-
-		//视图容器
-		private LayoutInflater listContainer;
-
-		// 每一个动态在主页上显示的内容分，
-		public final class ListItemsView {
-
-			public ImageView photoHead;
-
-			public TextView userName;
-
-			public ImageView location;
-
-			public TextView userAddress;
-
-			public TextView dasyaAgo;
-			public ImageView clockImage;
-
-			public TextView photoDescribe;
-
-			public ImageView showingPicture;
-
-			/**
-			 * 该照片被喜欢的人数
-			 */
-
-			public TextView likeSum;
-			/**
-			 * “Like” 提供点击进入查看那些人喜欢
-			 */
-			public TextView likeText;
-			/**
-			 * 该图片被评论的数量
-			 */
-			public TextView commentSum;
-			/**
-			 * “Comment”
-			 */
-			public TextView theCommentText;
-
-			// 喜欢等图片按钮
-			public ImageView loveHeartImage;
-			public ImageView commentImage;
-			public ImageView sharImage;
-			public ImageView moreMenu;
-
-		}
-
-		public MyContentListViewAdapter(Context context, List<HashMap<String, Object>> content) {
-
-			listContainer = LayoutInflater.from(context);
-			this.listData = content;
-		}
-
-		@Override
-		public int getCount() {
-
-			return listData.size();
-		}
-
-		@Override
-		public Object getItem(int position) {
-
-			return listData.get(position);
-		}
-
-		@Override
-		public long getItemId(int position) {
-
-			return position;
-		}
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-
-			ListItemsView listItemsView;
-
-			if (convertView == null) {
-				listItemsView = new ListItemsView();
-				convertView = listContainer.inflate(R.layout.index_listview_content, null);
-				listItemsView.photoHead = (ImageView) convertView.findViewById(R.id.index_head_photo_thumbnail);
-				listItemsView.userName = (TextView) convertView.findViewById(R.id.userNameIndex);
-				listItemsView.userName.setTextScaleX(1.2f);
-				listItemsView.userAddress = (TextView) convertView.findViewById(R.id.address);
-				listItemsView.showingPicture = (ImageView) convertView.findViewById(R.id.showingPicture);
-				listItemsView.photoDescribe = (TextView) convertView.findViewById(R.id.index_photo_describe);
-
-				// 设置控件集到convertView中
-				convertView.setTag(listItemsView);
-			} else {
-				listItemsView = (ListItemsView) convertView.getTag();
-			}
-
-			HashMap<String, Object> map = listData.get(position);
-			listItemsView.photoHead.setImageResource((Integer) map.get("photoHead"));
-			listItemsView.userName.setText((String) map.get("userName"));
-			listItemsView.userAddress.setText((String) map.get("userAddress"));
-			listItemsView.showingPicture.setImageResource((Integer) map.get("userPicture"));
-			listItemsView.photoDescribe.setText((String) map.get("explain"));
-
-			return convertView;
-		}
-	}
-
-	private List<HashMap<String, Object>> getListItems() {
-
-		items = new ArrayList<HashMap<String, Object>>();
-
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("photoHead", R.drawable.head2);
-		map.put("userName", "喜欢");
-		map.put("userAddress", "好望角海域");
-		map.put("userPicture", R.drawable.index_aother_user_picture);
-		map.put("explain", "这里是用户文字描述");
-		items.add(map);
-
-		HashMap<String, Object> map2 = new HashMap<String, Object>();
-		map2.put("photoHead", R.drawable.head2);
-		map2.put("userName", "不喜欢");
-		map2.put("userAddress", "成都双流");
-		map2.put("userPicture", R.drawable.index_aother_user_picture);
-		map2.put("explain", "我说了很多？");
-		items.add(map2);
-
-
-		HashMap<String, Object> map3 = new HashMap<String, Object>();
-		map3.put("photoHead", R.drawable.application);
-		map3.put("userName", "hellen");
-		map3.put("userAddress", "北京");
-		map3.put("userPicture", R.drawable.index_aother_user_picture);
-		map3.put("explain", "我说了很多aassdfsfdgsdg2222sdasd");
-		items.add(map3);
-		return items;
-	}
-
-	/**
-	 * 将页面添加到viewpager里面去
-	 */
-	private class myViewPagerAdapter extends PagerAdapter {
-
-		private List<View> listView;
-		int tagFlag = 1;
-
-		public myViewPagerAdapter(List<View> listView) {
-
-			this.listView = listView;
-		}
-
-		@Override
-		public void destroyItem(ViewGroup container, int position, Object object) {
-
-			container.removeView(listView.get(position));
-		}
-
-		@Override
-		public Object instantiateItem(ViewGroup container, int position) {
-
-			View v = listView.get(position);
-			v.setTag("tag" + tagFlag++);
-			container.addView(v, 0);
-			return listView.get(position);
-		}
-
-		@Override
-		public int getCount() {
-
-			return listView.size();
-		}
-
-		@Override
-		public boolean isViewFromObject(View arg0, Object arg1) {
-
-			return arg0 == arg1;
-		}
-	}
-
-	private static List<HashMap<String, Object>> items = null;
-
-	public static IndexPullRefreshListView indexPullListView;
-
-	static MyContentListViewAdapter myContentListViewAdapter;
-
-
-	private class GetDataTask extends AsyncTask<Void, Void, HashMap<String,Object>> {
-
-		private Context context;
-		private int index;
-
-		public GetDataTask(Context context, int index) {
-
-			this.context = context;
-			this.index = index;
-		}
-
-		@Override
-		protected HashMap<String,Object> doInBackground(Void... params) {
-			// Simulates a background job.
-			HashMap<String, Object> dataMap = new HashMap<String, Object>();
-			try {
-				Thread.sleep(2000);
-				dataMap.put("photoHead", R.drawable.head2);
-				dataMap.put("userName", String.valueOf(name++));
-				dataMap.put("userAddress", "好莱坞----");
-				dataMap.put("userPicture", R.drawable.index_aother_user_picture);
-				dataMap.put("explain", "用户文字描述");
-
-			} catch (InterruptedException e) {
-				Log.d(TAG, "Sleep Exception");
-			}
-
-			return dataMap;
-		}
-
-		@Override
-		protected void onPostExecute(HashMap<String,Object> result) {
-			if (index == PULL_TO_REFRESH) {
-				// 将字符串“Added after refresh”添加到顶部
-				//mListItems.addFirst("","Added after refresh...");
-				items.add(0, result);
-				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd  HH:mm");
-				String date = format.format(new Date());
-				// Call onRefreshComplete when the list has been refreshed.
-				indexPullListView.onRefreshComplete(date);
-
-			} else if (index == LOAD_MORE) {
-				items.add(items.size(), result);
-				indexPullListView.onLoadMoreComplete();
-			}
-//			myContentListViewAdapter.notifyDataSetChanged();
-		}
-		@Override
-		protected void onCancelled(HashMap<String, Object> stringObjectHashMap) {
-			super.onCancelled(stringObjectHashMap);
-		}
-	}
-	static int name = 10;
 }
